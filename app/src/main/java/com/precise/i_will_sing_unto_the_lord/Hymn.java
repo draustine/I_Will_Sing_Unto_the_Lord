0129b77610bf9b794448788daef280e51e9e3ace
draustine <draustine@yahoo.com>
2022-12-22 21:26:44 +0100

    Initial commit

---
+++ app/src/main/java/com/precise/i_will_sing_unto_the_lord/Hymn.java
@@ -0,0 +1,20 @@
+package com.precise.i_will_sing_unto_the_lord;
+
+public class Hymn {
+
+    private String title;
+    private String text;
+
+    public Hymn(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ app/src/main/java/com/precise/i_will_sing_unto_the_lord/MainActivity.java
@@ -0,0 +1,14 @@
+package com.precise.i_will_sing_unto_the_lord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
